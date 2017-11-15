41af475363e971456de6aa1a8fd1679754936f42
Jakub Beránek <berykubik@gmail.com>
2017-11-15 13:00:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# CPP to WASM Webpack loader
+Ever wanted to include C/C++ files directly from Javascript?
+Well now you can.
+
+This Webpack loader allows you to import C or C++ files, which are
+compiled to WASM using Emscripten.
+
+The C/C++ files have to know about Emscripten, so you can export functions. For simple
+things it's enough to include emscripten and add EMSCRIPTEN_KEEPALIVE to your functions
+(+ extern "C" for C++).
+
+## Usage
+Webpack settings
+```js
+module.exports = {
+  // ...
+  module: {
+    rules: [
+      {
+        test: /\.(c|cpp)$/,
+        use: {
+          loader: 'cpp-wasm-loader',
+          options: {
+            // Path to your 'build' or 'dist' directory relative to project root
+            buildPath: 'build',
+            emccPath: 'path-to-emcc',
+            emccFlags: ['-O3']
+          }
+        }
+      },
+      // ...
+    ]
+  }
+}
+```
+
+C/C++ file
+```c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <emscripten/emscripten.h>
+
+int main(int argc, char ** argv)
+{
+    printf("WASM loaded\n");
+}
+
+// Simple C function that returns a number between 1 and 6.
+int EMSCRIPTEN_KEEPALIVE roll_dice() {
+    srand ( time(NULL) );
+    return rand() % 6 + 1;
+}
+```
+
+Usage in Javascript
+```js
+import wasm from 'source.c';
+
+module.initialize().then(module => {
+  const result = module._roll_dice();
+  console.log(result);
+});
+```
+
+### Configuration
+The following options can be added to the Webpack loader query:
+
+| Name | Description | Required | Default |
+| ---- | ----------- | -------- | ------- |
+| `buildPath` |  Path to your 'build' or 'dist' directory relative to project root | true| |
+| `emccPath` | Path to your Emscripten binary (emcc or em++) | false | emcc |
+| `emccFlags` | Array of compilation flags passed to Esmcripten | false | ['-O3'] |
+
+### Requirements
+Emscripten
+
+#### License
+MIT
+
+#### Ideas
+* generate Typescript definitions (using Clang)
+* automatically export functions (using Clang)
+
+#### Inspiration
+Inspired by [rust-wasm-loader](https://www.npmjs.com/package/rust-wasm-loader) and
+[this article](https://tutorialzine.com/2017/06/getting-started-with-web-assembly).
+WASM alternative of [cpp-loader](https://www.npmjs.com/package/cpp-loader).
---
+++ src/index.js
@@ -0,0 +1,91 @@
+import Promise from 'bluebird';
+import { loadOptions } from './options';
+
+const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const tmp = require('tmp');
+const rimraf = require('rimraf');
+const md5 = require('md5');
+
+const tmpDir = Promise.promisify(tmp.dir);
+const readFile = Promise.promisify(fs.readFile);
+const writeFile = Promise.promisify(fs.writeFile);
+const execFile = Promise.promisify(cp.execFile);
+const rf = Promise.promisify(rimraf);
+
+function buildModule(wasmName, indexContent) {
+  const Module = {
+    wasmBinaryFile: wasmName,
+    ENVIRONMENT: 'WEB',
+  };
+
+  return `module.exports = (function(existingModule)
+          {
+              return {
+                initialize: function(userDefinedModule)
+                {
+                  return new Promise((resolve, reject) =>
+                  {
+                    if (!userDefinedModule)
+                    {
+                      userDefinedModule = {}
+                    }
+                    var Module = Object.assign({}, userDefinedModule, existingModule);
+                    Module['onRuntimeInitialized'] = () => resolve(Module);
+                    \n${indexContent}\n
+                  });
+                }
+              }
+            })(${JSON.stringify(Module)})`;
+}
+
+function createBuildWasmName(resource, content) {
+  const fileName = path.basename(resource, path.extname(resource));
+  return `${fileName}-${md5(content)}.wasm`;
+}
+
+export default async function loader(content) {
+  const cb = this.async();
+  let folder = null;
+
+  try {
+    const options = loadOptions(this);
+
+    const inputFile = `input${path.extname(this.resourcePath)}`;
+    const indexFile = 'index.js';
+    const wasmFile = 'index.wasm';
+
+    options.emccFlags = [inputFile, '-s', 'WASM=1', ...options.emccFlags, '-o', indexFile];
+
+    folder = await tmpDir();
+
+    // write source to tmp directory
+    await writeFile(path.join(folder, inputFile), content);
+
+    // compile source file to WASM
+    await execFile(options.emccPath, options.emccFlags, {
+      cwd: folder,
+    });
+
+    const indexContent = await readFile(path.join(folder, indexFile), 'utf8');
+    const wasmContent = await readFile(path.join(folder, wasmFile));
+
+    const wasmBuildName = createBuildWasmName(this.resourcePath, content);
+    this.emitFile(wasmBuildName, wasmContent);
+
+    const module = buildModule(wasmBuildName, indexContent);
+
+    if (folder !== null) {
+      await rf(folder);
+    }
+    cb(null, module);
+  } catch (e) {
+    if (folder !== null) {
+      await rf(folder);
+    }
+    cb(e);
+  }
+
+  return null;
+}
